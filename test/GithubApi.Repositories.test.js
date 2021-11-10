f501ababb719604a0cfc9cb348133d7b32037d95
salitomeyo <salitomeyo16@gmail.com>
2021-11-10 13:46:29 -0500

    get request

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -1,12 +1,17 @@
 const agent = require('superagent');
 const statusCode = require('http-status-codes');
 const { expect } = require('chai');
+const chai = require('chai');
+const chaiSubset = require('chai-subset');
+
+chai.use(chaiSubset);
 
 const urlBase = 'https://api.github.com';
 const githubUserName = 'aperdomob';
 
 describe('Github Api Test', () => {
   it('Checking name', async () => {
+    // https://api.github.com/users/aperdomob
     const response = await agent.get(`${urlBase}/users/${githubUserName}`)
       .auth('token', process.env.ACCESS_TOKEN)
       .set('User-Agent', 'agent');
@@ -17,6 +22,7 @@ describe('Github Api Test', () => {
   });
 
   it('Checking company', async () => {
+    // https://api.github.com/users/aperdomob
     const response = await agent.get(`${urlBase}/users/${githubUserName}`)
       .auth('token', process.env.ACCESS_TOKEN)
       .set('User-Agent', 'agent');
@@ -27,6 +33,7 @@ describe('Github Api Test', () => {
   });
 
   it('Checking location', async () => {
+    // https://api.github.com/users/aperdomob
     const response = await agent.get(`${urlBase}/users/${githubUserName}`)
       .auth('token', process.env.ACCESS_TOKEN)
       .set('User-Agent', 'agent');
@@ -40,6 +47,7 @@ describe('Github Api Test', () => {
 describe('Repo jasmine-awesome-report', () => {
   let repo;
   it('finding repo url', async () => {
+    // https://api.github.com/users/aperdomob/repos
     const response = await agent.get(`https://api.github.com/users/${githubUserName}/repos`)
       .auth('token', process.env.ACCESS_TOKEN)
       .set('User-Agent', 'agent');
@@ -50,62 +58,71 @@ describe('Repo jasmine-awesome-report', () => {
   });
 
   it('Checking full name', async () => {
-    const respon = await agent.get(`${repo.url}`)
-      .auth('token', process.env.ACCESS_TOKEN)
-      .set('User-Agent', 'agent');
-
-    expect(respon.status).to.equal(statusCode.OK);
-    expect(respon.body).to.have.property('full_name');
-    expect(respon.body.full_name).equal('aperdomob/jasmine-awesome-report');
-  });
-
-  it('Checking private', async () => {
-    const respon = await agent.get(`${repo.url}`)
-      .auth('token', process.env.ACCESS_TOKEN)
-      .set('User-Agent', 'agent');
-
-    expect(respon.status).to.equal(statusCode.OK);
-    expect(respon.body).to.have.property('private');
-    expect(respon.body.private).equal(false);
-  });
-
-  it('Checking description', async () => {
-    const respon = await agent.get(`${repo.url}`)
-      .auth('token', process.env.ACCESS_TOKEN)
-      .set('User-Agent', 'agent');
-
-    expect(respon.status).to.equal(statusCode.OK);
-    expect(respon.body).to.have.property('description');
-    expect(respon.body.description).equal('An awesome html report for Jasmine');
-  });
-
-  it('Getting archive list', async () => {
-    const respon = await agent.get(`${repo.url}`)
-      .auth('token', process.env.ACCESS_TOKEN)
-      .set('User-Agent', 'agent');
-
-    expect(respon.status).to.equal(statusCode.OK);
-    expect(respon.body).to.have.property('archive_url');
-  });
-});
-
-describe('Download jasmine-awesome-report', () => {
-  it('Getting archive url', async () => {
-    const response = await agent.get('https://api.github.com/repos/aperdomob/jasmine-awesome-report')
+    // https://api.github.com/repos/aperdomob/jasmine-awesome-report
+    const response = await agent.get(`${repo.url}`)
       .auth('token', process.env.ACCESS_TOKEN)
       .set('User-Agent', 'agent');
 
     expect(response.status).to.equal(statusCode.OK);
-    expect(response.body).to.have.property('archive_url');
+    expect(response.body).to.have.property('full_name');
+    expect(response.body.full_name).equal('aperdomob/jasmine-awesome-report');
+  });
 
-    it('Getting archive list', async () => {
-      const respon = await agent.get(`${response.archive_url}`)
-        .auth('token', process.env.ACCESS_TOKEN)
-        .set('User-Agent', 'agent');
+  it('Checking private', async () => {
+    // https://api.github.com/repos/aperdomob/jasmine-awesome-report
+    const response = await agent.get(`${repo.url}`)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .set('User-Agent', 'agent');
 
-      expect(respon.status).to.equal(statusCode.OK);
-      const repo = respon.body.find((element) => element.name === 'README.md');
-      expect(repo);
-    });
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.have.property('private');
+    expect(response.body.private).equal(false);
+  });
+
+  it('Checking description', async () => {
+    // https://api.github.com/repos/aperdomob/jasmine-awesome-report
+    const response = await agent.get(`${repo.url}`)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .set('User-Agent', 'agent');
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.have.property('description');
+    expect(response.body.description).equal('An awesome html report for Jasmine');
+  });
+
+  it('Getting archive list', async () => {
+    // https://api.github.com/repos/aperdomob/jasmine-awesome-report
+    const response = await agent.get(`${repo.url}`)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .set('User-Agent', 'agent');
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.have.property('contents_url');
+  });
+});
+
+describe('README.md file comprobations', () => {
+  let repo;
+  it('Getting README.md', async () => {
+    const response = await agent.get('https://api.github.com/repos/aperdomob/jasmine-awesome-report/contents')
+      .auth('token', process.env.ACCESS_TOKEN)
+      .set('User-Agent', 'agent');
+
+    expect(response.status).to.equal(statusCode.OK);
+    repo = response.body.find((element) => element.name === 'README.md');
+    expect(repo);
+  });
+
+  it('checking README.md name, path, sha with chaiSubset', async () => {
+    // https://api.github.com/repos/aperdomob/jasmine-awesome-report/contents/README.md?ref=master
+    const response = await agent.get(`${repo.url}`)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .set('User-Agent', 'agent');
+
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.have.property('name');
+    expect(response.body).to.have.property('path');
+    expect(response.body).to.have.property('sha');
+    expect(response.body).to.containSubset({ name: 'README.md', path: 'README.md', sha: '1eb7c4c6f8746fcb3d8767eca780d4f6c393c484' });
   });
 });
